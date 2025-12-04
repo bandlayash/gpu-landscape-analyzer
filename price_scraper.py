699c5b6e9ab4a1f62d9fde157846f05b76a7e201
Yash Bandla <bandla.yash@gmail.com>
2025-12-03 22:34:27 -0600

    fixed price_scraper.py to ignore vendor name

--- price_scraper.py
+++ price_scraper.py
@@ -16,6 +16,24 @@ try:
 except sqlite3.OperationalError:
     print(f"Column 'launch_prices' already exists.")
 
+
+
+def clean_gpu_name(scraped_name):
+    # 1. Remove "Specs" from the end
+    name = scraped_name.replace(" Specs", "").strip()
+    
+    # 2. List of vendors to remove from the start
+    vendors = ["NVIDIA ", "AMD ", "Intel ", "ATI "]
+    
+    for vendor in vendors:
+        if name.startswith(vendor):
+            name = name.replace(vendor, "", 1) # Remove only the first occurrence
+            break
+            
+    return name.strip()
+
+
+
 driver = webdriver.Chrome()
 driver.get("https://www.techpowerup.com/gpu-specs/")
 links = []
@@ -46,7 +64,7 @@ for index, link in enumerate(links):
         except:
             launch_price = "Not Found"
         full_title = driver.title.split('|')[0].strip()
-        clean_name = full_title.replace(" Specs", "").strip()
+        clean_name = clean_gpu_name(full_title)
 
         print(f"[{index + 1}/{len(links)}] {clean_name} -> {launch_price}")
 
